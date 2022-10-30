0d13343986688c71fb90957f3e075e762671c566
Devraj Kumar <109935648+drs317lakshami@users.noreply.github.com>
2022-10-30 14:21:31 +0530

    Solving using Switch Case Statement

--- WelcomeToEmployeeWagesProgram.java
+++ WelcomeToEmployeeWagesProgram.java
@@ -1,29 +1,31 @@
 package com.blz.shubham.employeewages;
 
-public class PartTimeEmployeeAndWage{
-    public static void main(String args[])
-    {
+public class SolvingUsingSwitchCaseStatement{
+       public static void main(String args[]) {
 
-    final int PART_TIME = 1;
-    final int FULL_TIME = 2;
-    final int Wage_Per_Hr = 20;
-    int empType = (int) (Math.random() * 100) % 3;
-    int workingHours = 0;
-        if (empType == FULL_TIME)
-    {
-        System.out.println("Employee is Present Full Time");
-        workingHours = 8; //Assume Part time Hour is 8
-    }
-        else if (empType == PART_TIME)
-        {
-            System.out.println("Employee is Present Part Time");
-            workingHours = 4;
+        final int PART_TIME = 1;
+        final int FULL_TIME = 2;
+        final int Wage_Per_Hr = 20;
+        int empType = (int) (Math.random() * 100) % 3;
+        int workingHours = 0;
+        switch (empType) {
+            case FULL_TIME:
+                System.out.println("Employee is Present Full Time");
+                workingHours = 8;
+                break;
+
+            case PART_TIME:
+                System.out.println("Employee is Present Part Time");
+                workingHours = 4;
+                break;
+
+            default:
+                System.out.println("Employee is Absent");
+
         }
-        else	
-    {
-        System.out.println("Employee is Absent");
-    }
-    int wage = workingHours * Wage_Per_Hr;
-        System.out.println("Employee Daily Wage is " + wage);
+                int wage = workingHours * Wage_Per_Hr;
+                System.out.println("Employee Daily Wage is " + wage);
+
+        
     }
 }

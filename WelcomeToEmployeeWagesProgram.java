f39e6bdb934cfe0583d4e72efb393e5e7a5465f3
Devraj Kumar <109935648+drs317lakshami@users.noreply.github.com>
2022-10-30 13:45:00 +0530

    Add Part time Employee & Wage :Assume Part time Hour is 8

--- WelcomeToEmployeeWagesProgram.java
+++ WelcomeToEmployeeWagesProgram.java
@@ -1,19 +1,25 @@
 package com.blz.shubham.employeewages;
 //- Assume Wage per Hour is 20 - Assume Full Day Hour is 8
-public class CalculateDailyEmployeeWage {
-  public static void main(String args[])
+public class PartTimeEmployeeAndWage{
+    public static void main(String args[])
     {
 
-    final int FULL_TIME = 1;
+    final int PART_TIME = 1;
+    final int FULL_TIME = 2;
     final int Wage_Per_Hr = 20;
-    int empType = (int) (Math.random() * 100) % 2;
+    int empType = (int) (Math.random() * 100) % 3;
     int workingHours = 0;
         if (empType == FULL_TIME)
     {
-        System.out.println("Employee is Present");
+        System.out.println("Employee is Present Full Time");
         workingHours = 8;
     }
-        else
+        else if (empType == PART_TIME)
+        {
+            System.out.println("Employee is Present Part Time");
+            workingHours = 4;
+        }
+        else	
     {
         System.out.println("Employee is Absent");
     }

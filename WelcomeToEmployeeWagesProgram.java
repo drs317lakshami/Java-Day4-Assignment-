261c8b6672e742670d5cb36f7a147be9176c0846
Devraj Kumar <109935648+drs317lakshami@users.noreply.github.com>
2022-10-30 13:25:09 +0530

    Welcome to Employee Wage Computation Program on Master Branch

--- WelcomeToEmployeeWagesProgram.java
+++ WelcomeToEmployeeWagesProgram.java
@@ -2,6 +2,6 @@ package com.blz.shubham.employeewages;
 
 public class WelcomeToEmployeeWagesProgram {
 	public static void main(String[] args) {
-		System.out.println("Warm Welcome to employee wages program");
+		System.out.println("Warm Welcome to employee wages programs");
 	}
 }

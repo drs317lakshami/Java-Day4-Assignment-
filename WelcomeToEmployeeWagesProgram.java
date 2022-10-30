630a9411a6f25e7bb295ac8a217d3853356bdda3
drs317lakshami <devrajkumar317@gmail.com>
2022-10-30 13:22:19 +0530

    Welcome to Employee

---
+++ WelcomeToEmployeeWagesProgram.java
@@ -0,0 +1,7 @@
+package com.blz.shubham.employeewages;
+
+public class WelcomeToEmployeeWagesProgram {
+	public static void main(String[] args) {
+		System.out.println("Warm Welcome to employee wages program");
+	}
+}

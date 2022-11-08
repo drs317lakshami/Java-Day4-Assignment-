2787416a93150247331dd487614022d6cd178e10
Devraj Kumar <109935648+drs317lakshami@users.noreply.github.com>
2022-11-08 09:02:16 +0530

    Added: Welcome to employee wages program

--- WelcomeToEmployeeWagesProgram.java
+++ WelcomeToEmployeeWagesProgram.java
@@ -1,7 +1,9 @@
 package com.blz.shubham.employeewages;
 
 public class WelcomeToEmployeeWagesProgram {
+	
 	public static void main(String[] args) {
+		
 		System.out.println("Warm Welcome to employee wages programs");
 	}
 }
